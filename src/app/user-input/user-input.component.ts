bd7ea2ff6b5c518936977399846e342f03f3a238
andre_zktt <andrezicatti@gmail.com>
2025-04-24 17:55:32 -0300

    :sparkles: feat: Form Handling

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {InvestmentInput} from "../investment-input.model";
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  entryInitialInvestment = '0';
+  entryAnnualInvestment = '0';
+  entryExpectedReturn = '5';
+  entryDuration = '10';
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.entryInitialInvestment,
+      annualInvestment: +this.entryAnnualInvestment,
+      expectedReturn: +this.entryExpectedReturn,
+      duration: +this.entryDuration
+    });
+  }
 
 }
